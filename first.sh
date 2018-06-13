e5de904420b37178641f334fbe7dbbf3aba705de
Cyruswen <Cyrus_wen@163.com>
2018-06-13 18:14:42 +0800

    updata first.sh

--- first.sh
+++ first.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 #echo "hello world!"
 
-pwd
-cd ..
-pwd
+#pwd
+#cd ..
+#pwd
+
+#myint=1000
+#myfloat=3.14
+#mystring="hello world"
+#mychar='c'
+#
+#echo $myint
+#echo $myfloat
+#echo $mystring
+#echo $mychar
+
+#mystring="hello world"
+#
+#echo "${mystring}hello bit"
+
+#your_hobby="code"
+#echo "$your_hobby"
+#unset your_hobby
+#echo "$your_hobby"
+
+echo $myvalue
